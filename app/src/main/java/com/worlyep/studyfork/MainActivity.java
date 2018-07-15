b75dc82e9a8d562701463990b7934aabef546935
sgc109 <sgc109109@gmail.com>
2018-07-15 21:03:17 +0900

    스터디가 끝난 직후 커밋

--- app/src/main/java/com/worlyep/studyfork/MainActivity.java
+++ app/src/main/java/com/worlyep/studyfork/MainActivity.java
@@ -2,6 +2,9 @@ package com.worlyep.studyfork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
---
+++ app/src/main/java/com/worlyep/studyfork/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,27 @@
+package com.worlyep.studyfork;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+
+    public SimpleFragmentPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
