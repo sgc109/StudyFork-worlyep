13efcdfb513005d187727a34807fbcb3e4063eeb
worlyep <worlyep@ogqcorp.com>
2018-07-15 15:36:19 +0900

    git init

---
+++ app/src/main/java/com/worlyep/studyfork/MainActivity.java
@@ -0,0 +1,15 @@
+package com.worlyep.studyfork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
