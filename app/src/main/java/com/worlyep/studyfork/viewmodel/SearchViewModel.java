6c061ff2c7e7c0f9cfb7718c20bc01409c5c4009
sgc109 <sgc109109@gmail.com>
2018-07-28 19:50:01 +0900

    ViewModel 말고 AndroidViewModel 을 상속받게 해서 Application Context 넘겨줌 이렇게 하면 Application 과 lifecycle을 같이하고 Application Context 에 지속적으로 접근할 수 있는듯

--- app/src/main/java/com/worlyep/studyfork/viewmodel/SearchViewModel.java
+++ app/src/main/java/com/worlyep/studyfork/viewmodel/SearchViewModel.java
@@ -1,18 +1,21 @@
 package com.worlyep.studyfork.viewmodel;
 
 import android.app.Activity;
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.ViewModel;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.View;
 
-public class SearchViewModel implements ViewModel{
-    public Context context;
+public class SearchViewModel extends AndroidViewModel implements BaseViewModel{
 
     public View.OnClickListener onBackClick = view -> {
-        ((Activity)this.context).finish();
+        ((Activity)view.getContext()).finish();
     };
 
-    public SearchViewModel(Context context){
-        this.context = context;
+    public SearchViewModel(@NonNull Application application) {
+        super(application);
     }
 
     @Override
