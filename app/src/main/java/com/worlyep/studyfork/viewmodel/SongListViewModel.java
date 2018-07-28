e5bbaa6e7845ecf7c6cc3222b8e423a8bc121993
sgc109 <sgc109109@gmail.com>
2018-07-28 19:47:14 +0900

    Repository 에서 Retrofit2 로 가져와 만들게한 LiveData 받아서 LievData 필드에 넣어주는 코드

--- app/src/main/java/com/worlyep/studyfork/viewmodel/SongListViewModel.java
+++ app/src/main/java/com/worlyep/studyfork/viewmodel/SongListViewModel.java
@@ -1,6 +1,31 @@
 package com.worlyep.studyfork.viewmodel;
 
-public class SongListViewModel implements ViewModel{
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableBoolean;
+import android.support.annotation.NonNull;
+
+import com.worlyep.studyfork.model.Song;
+import com.worlyep.studyfork.repository.SongRepository;
+
+import java.util.List;
+
+public class SongListViewModel extends AndroidViewModel implements BaseViewModel{
+    private final LiveData<List<Song>> songListObservable;
+    ObservableBoolean progBarVisib = new ObservableBoolean();
+
+    public SongListViewModel(@NonNull Application application) {
+        super(application);
+
+        songListObservable = SongRepository.getInstance().getSongList();
+    }
+
+    public LiveData<List<Song>> getSongListObservable() {
+        return songListObservable;
+    }
+
     @Override
     public void onCreate() {
 
