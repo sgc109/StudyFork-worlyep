51d9fcbeab241b10169533967fcc0fa67f8d445b
sgc109 <sgc109109@gmail.com>
2018-07-28 20:15:49 +0900

    PagerAdapter 를 MainViewModel 에서 만들어 등록해주는 부분을 MainActivity 로 뺌. 왜냐면 ViewModel 은 안드로이드에 대해 몰라야하기 때문에? Activity 의 context 를 ViewModel 이 들고있으면 안좋다는 말을 보기도했고(맞는진 모르겠음) 어댑터를 뷰모델이 아니라 뷰쪽의 클래스에 두는걸 몇번 봐서. 그리고 어댑터가 뷰쪽에 갔기 때문에 Activity에서 Adapter를 데이터바인딩시켜서 adapter 를 코드가 아닌 데이터바인딩으로 등록함

--- app/src/main/java/com/worlyep/studyfork/view/activity/MainActivity.java
+++ app/src/main/java/com/worlyep/studyfork/view/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.worlyep.studyfork.view.activity;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
@@ -17,18 +19,21 @@ import com.worlyep.studyfork.view.adapter.SimpleFragmentPagerAdapter;
 import com.worlyep.studyfork.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
-
-    MainViewModel mViewModel;
+    public PagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mViewModel = new MainViewModel(this);
+
+        final MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.setViewmodel(mViewModel);
+        binding.setViewmodel(viewModel);
 
         ViewPager viewPager = binding.viewpager;
         TabLayout tabLayout = binding.tabLayout;
         tabLayout.setupWithViewPager(viewPager);
+
+        adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager(), getApplication());
+        binding.setAdapter(adapter);
     }
 }
